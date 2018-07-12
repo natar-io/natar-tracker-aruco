4d120f0d93301355437cf349e4dc1e3a01639ac8
Nicolas PALARD <palardnicolas@gmail.com>
2018-07-12 09:05:23 +0200

    :sparkles: Added full command line options implementation & fixed missing files

--- src/server.cpp
+++ src/server.cpp
@@ -10,7 +10,134 @@
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 
+#include <cxxopts.hpp>
+#include <RedisImageHelper.hpp>
+
+bool VERBOSE = false;
+
+bool STREAM_MODE = false;
+bool SET_MODE = false;
+
+std::string redisInputKey = "camera0";
+std::string redisOutputKey = "camera0:markers";
+std::string redisInputCameraParametersKey = "camera0";
+
+std::string redisHost = "127.0.0.1";
+int redisPort = 6379;
+
 using namespace cv;
+
+struct contextData {
+    uint width;
+    uint height;
+    uint channels;
+    RedisImageHelperSync* clientSync;
+};
+
+Ptr<aruco::Dictionary> DICTIONARY = aruco::getPredefinedDictionary(aruco::DICT_ARUCO_ORIGINAL);
+
+static int parseCommandLine(cxxopts::Options options, int argc, char** argv)
+{
+    auto result = options.parse(argc, argv);
+    if (result.count("h")) {
+        std::cout << options.help({"", "Group"});
+        return EXIT_FAILURE;
+    }
+
+    if (result.count("v")) {
+        VERBOSE = true;
+        std::cerr << "Verbose mode enabled." << std::endl;
+    }
+
+    if (result.count("i")) {
+        redisInputKey = result["i"].as<std::string>();
+        if (VERBOSE) {
+            std::cerr << "Input key was set to `" << redisInputKey << "`." << std::endl;
+        }
+    }
+    else {
+        if (VERBOSE) {
+            std::cerr << "No input key was specified. Input key was set to default (" << redisInputKey << ")." << std::endl;
+        }
+    }
+
+    if (result.count("o")) {
+        redisOutputKey = result["o"].as<std::string>();
+        if (VERBOSE) {
+            std::cerr << "Output key was set to `" << redisOutputKey << "`." << std::endl;
+        }
+    }
+    else {
+        if (VERBOSE) {
+            std::cerr << "No output key was specified. Output key was set to default (" << redisOutputKey << ")." << std::endl;
+        }
+    }
+
+    if (result.count("u")) {
+        STREAM_MODE = false;
+        SET_MODE = false;
+        if (VERBOSE) {
+            std::cerr << "Unique mode enabled." << std::endl;
+        }
+    }
+
+    if (result.count("s")) {
+        STREAM_MODE = true;
+        if (VERBOSE) {
+            std::cerr << "PUBLISH stream mode enabled." << std::endl;
+        }
+    }
+
+    if (result.count("g")) {
+        SET_MODE = true;
+        if (VERBOSE) {
+            std::cerr << "GET/SET stream mode enabled." << std::endl;
+        }
+    }
+
+    if (result.count("redis-port")) {
+        redisPort = result["redis-port"].as<int>();
+        if (VERBOSE) {
+            std::cerr << "Redis port set to " << redisPort << "." << std::endl;
+        }
+    }
+    else {
+        if (VERBOSE) {
+            std::cerr << "No redis port specified. Redis port was set to " << redisPort << "." << std::endl;
+        }
+    }
+
+    if (result.count("redis-host")) {
+        redisHost = result["redis-host"].as<std::string>();
+        if (VERBOSE) {
+            std::cerr << "Redis host set to " << redisHost << "." << std::endl;
+        }
+    }
+    else {
+        if (VERBOSE) {
+            std::cerr << "No redis host was specified. Redis host was set to " << redisHost << "." << std::endl;
+        }
+    }
+
+    if (result.count("camera-parameters")) {
+        redisInputCameraParametersKey = result["camera-parameters"].as<std::string>();
+        if (VERBOSE) {
+            std::cerr << "Camera parameters input key was set to " << redisInputCameraParametersKey << std::endl;
+        }
+    }
+    else {
+        if (VERBOSE) {
+            std::cerr << "No camera parameters intput key specified. Camera parameters input key was set to " << redisInputCameraParametersKey << std::endl;
+        }
+    }
+
+    if (!result.count("u") && !result.count("s") && !result.count("g")) {
+        std::cerr << "You need to specify at least the stream method option with -u, -s or -g" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
 
 rapidjson::Value* ARUCOMarkerToJSON(int id, int dir, int confidence, float* center, std::vector<Point2f>corners, rapidjson::Document::AllocatorType& allocator) {
     rapidjson::Value* markerObj = new rapidjson::Value(rapidjson::kObjectType);
@@ -56,22 +183,11 @@ rapidjson::Value* ARUCOMarkersToJSON(std::vector<int> ids, std::vector<std::vect
     return markersObj;
 }
 
-int main(int argc, char** argv) {
-    Ptr<aruco::DetectorParameters> detectorParams = aruco::DetectorParameters::create();
-    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_ARUCO_ORIGINAL);
+std::string process(Image* image) {
     std::vector<int> ids;
-    std::vector<std::vector<Point2f> > corners, rejected;
-    Mat image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    Mat imageCopy; image.copyTo(imageCopy);
-
-    aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
-
-    if (ids.size() > 0) {
-        aruco::drawDetectedMarkers(imageCopy, corners, ids);
-    } else {
-        std::cerr << "No aruco markers found" << std::endl;
-        aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
-    }
+    std::vector<std::vector<Point2f> > corners;
+    cv::Mat imageCv(image->height(), image->width(), CV_8UC3, image->data());
+    aruco::detectMarkers(imageCv, DICTIONARY, corners, ids);
 
     rapidjson::Document jsonMarkers;
     jsonMarkers.SetObject();
@@ -84,8 +200,101 @@ int main(int argc, char** argv) {
     rapidjson::StringBuffer strbuf;
     rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
     jsonMarkers.Accept(writer);
-    std::cerr << strbuf.GetString() << std::endl;
+    return strbuf.GetString();
+}
 
-    imshow("out", imageCopy);
-    waitKey();
+void onImagePublished(redisAsyncContext* c, void* rep, void* privdata) {
+    redisReply *reply = (redisReply*) rep;
+    if  (reply == NULL) { return; }
+    if (reply->type != REDIS_REPLY_ARRAY || reply->elements != 3) {
+        if (VERBOSE) {
+            std::cerr << "Error: Bad reply format." << std::endl;
+        }
+        return;
+    }
+
+    struct contextData* data = static_cast<struct contextData*>(privdata);
+    if (data == NULL) {
+        if(VERBOSE) {
+            std::cerr << "Error: Could not retrieve context data from private data." << std::endl;
+        }
+        return;
+    }
+    uint width = data->width;
+    uint height = data->height;
+    uint channels = data->channels;
+    RedisImageHelperSync* clientSync = data->clientSync;
+
+    Image* image = RedisImageHelper::dataToImage(reply->element[2]->str, width, height, channels, reply->element[2]->len);
+    if (image == NULL) {
+        if (VERBOSE) {
+            std::cerr << "Error: Could not retrieve image from data." << std::endl;
+        }
+        return;
+    }
+    std::string json = process(image);
+    if (SET_MODE) {
+        clientSync->setString((char*)json.c_str(), redisOutputKey);
+    }
+    clientSync->publishString((char*)json.c_str(), redisOutputKey);
+    if (VERBOSE) {
+        std::cerr << json << std::endl;
+    }
+    delete image;
+}
+
+int main(int argc, char** argv) {
+    cxxopts::Options options("aruco-detection-server", "Aruco markers detection server.");
+    options.add_options()
+            ("redis-port", "The port to which the redis client should try to connect.", cxxopts::value<int>())
+            ("redis-host", "The host adress to which the redis client should try to connect", cxxopts::value<std::string>())
+            ("i, input", "The redis input key where data are going to arrive.", cxxopts::value<std::string>())
+            ("o, output", "The redis output key where to set output data.", cxxopts::value<std::string>())
+            ("s, stream", "Activate stream mode. In stream mode the program will constantly process input data and publish output data.")
+            ("u, unique", "Activate unique mode. In unique mode the program will only read and output data one time.")
+            ("g, stream-set", "Enable stream get/set mode. If stream mode is already enabled setting this will cause to publish and set the same data at the same time")
+            ("c, camera-parameters", "The redis input key where camera-parameters are going to arrive.", cxxopts::value<std::string>())
+            ("v, verbose", "Enable verbose mode. This will print helpfull process informations on the standard error stream.")
+            ("h, help", "Print this help message.");
+
+    if (parseCommandLine(options, argc, argv)) {
+        return EXIT_FAILURE;
+    }
+
+    RedisImageHelperSync clientSync(redisHost, redisPort, redisInputKey);
+    if (!clientSync.connect()) {
+        std::cerr << "Cannot connect to redis server. Please ensure that a redis server is up and running." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    struct contextData data;
+    data.width = clientSync.getInt(redisInputCameraParametersKey + ":width");
+    data.height = clientSync.getInt(redisInputCameraParametersKey + ":height");
+    data.channels = clientSync.getInt(redisInputCameraParametersKey + ":channels");
+    if (data.width == -1 || data.height == -1 || data.channels == -1) {
+        // TODO: Fix double free or corruption error when camera parameters can not be loaded.
+        std::cerr << "Could not find camera parameters (width height channels). Please specify where to find them in redis with the --camera-parameters option parameters." << std::endl;
+        return EXIT_FAILURE;
+    }
+    data.clientSync = &clientSync;
+
+    if (STREAM_MODE) {
+        RedisImageHelperAsync clientAsync(redisHost, redisPort, redisInputKey);
+        if (!clientAsync.connect()) {
+            std::cerr << "Cannot connect to redis server. Please ensure that a redis server is up and running." << std::endl;
+            return EXIT_FAILURE;
+        }
+        clientAsync.subscribe(redisInputKey, onImagePublished, static_cast<void*>(&data));
+    }
+    else {
+        bool loop = true;
+        while (loop) {
+            Image* image = clientSync.getImage(data.width, data.height, data.channels);
+            std::string json = process(image);
+            clientSync.setString((char*)json.c_str(), redisOutputKey);
+            std::cerr << json << std::endl;
+            delete image;
+            loop = SET_MODE ? true : false;
+        }
+    }
 }
