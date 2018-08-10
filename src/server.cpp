c86dfd2d776a0e91c8c6645e2d312115bb133787
Nicolas PALARD <palardnicolas@gmail.com>
2018-08-10 09:15:26 +0200

    :zap: Stream mode no more SUBSCRIBE to images but instead SUBSCRIBE to info and GET images when infos arrived

--- src/server.cpp
+++ src/server.cpp
@@ -13,17 +13,16 @@
 #include <cxxopts.hpp>
 #include <RedisImageHelper.hpp>
 
-bool VERBOSE = false;
+static bool VERBOSE = false;
+static bool STREAM_MODE = false;
+static bool SET_MODE = false;
 
-bool STREAM_MODE = false;
-bool SET_MODE = false;
+static std::string redisInputKey = "camera0";
+static std::string redisOutputKey = "camera0:markers";
+static std::string redisInputCameraParametersKey = "camera0";
 
-std::string redisInputKey = "camera0";
-std::string redisOutputKey = "camera0:markers";
-std::string redisInputCameraParametersKey = "camera0";
-
-std::string redisHost = "127.0.0.1";
-int redisPort = 6379;
+static std::string redisHost = "127.0.0.1";
+static int redisPort = 6379;
 
 using namespace cv;
 
@@ -235,7 +234,7 @@ void onImagePublished(redisAsyncContext* c, void* rep, void* privdata) {
     uint channels = data->channels;
     RedisImageHelperSync* clientSync = data->clientSync;
 
-    Image* image = RedisImageHelper::dataToImage(reply->element[2]->str, width, height, channels, reply->element[2]->len);
+    Image* image = clientSync->getImage(width, height, channels, redisInputKey);
     if (image == NULL) {
         if (VERBOSE) {
             std::cerr << "Error: Could not retrieve image from data." << std::endl;
